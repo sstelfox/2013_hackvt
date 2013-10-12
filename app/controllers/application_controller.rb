7a29077c61a25e9d3b86303642c5b5c847395f2b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 23:33:45 -0400

    Added authenticate_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authenticate_user
+    unless current_user
+      redirect_to login_path, notice: "You need to login before continuing."
+    end
+  end
+
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
