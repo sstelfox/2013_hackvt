b76acc5afd0f268b6ff02bca211b2162401e657c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-12 04:45:39 -0400

    Fixed scoping issue elsewhere

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -5,14 +5,14 @@ class IncidentsController < ApplicationController
   # GET /bikes/1/report_stolen
   # GET /report_stolen
   def new
-    bike = !!bike_id ? Bike.find(bike_id) : nil
+    bike = !!bike_id ? Bike.where(user: current_user).find(bike_id) : nil
     @incident_form = IncidentForm.new(bike)
   end
 
   # POST /bikes/1/report_stolen
   # POST /report_stolen
   def create
-    bike = !!bike_id ? Bike.find(bike_id) : nil
+    bike = !!bike_id ? Bike.where(user: current_user).find(bike_id) : nil
     @incident_form = IncidentForm.new(bike)
 
     if @incident_form.submit(incident_params)
