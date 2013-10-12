a78dbd9b91589d1622afc6463f7bf132548f4acb
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-12 00:05:33 -0400

    Stupid whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def current_user
     return nil unless session[:user_id]
     return session[:user_id] = nil unless User.exists?(id: session[:user_id])
-    User.find(session[:user_id]) 
+    User.find(session[:user_id])
   end
 end
