0ca5c23cb3cdfd1eb3199bb47e3a29c2f77a49ea
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 22:36:22 -0400

    That should do login / logout

--- app/forms/registration_form.rb
+++ app/forms/registration_form.rb
@@ -29,8 +29,8 @@ class RegistrationForm
 
   def verify_user_attributes
     unless user.valid?
-      user.errors.messages.each do |attr, msg|
-        errors.add(attr, msg)
+      user.errors.messages.each do |attr, msgs|
+        msgs.each { |m| errors.add(attr, m) }
       end
     end
   end
