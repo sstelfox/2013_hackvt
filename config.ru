c1ffc83befa2407815f2960b057df4e75918a8a7
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 20:01:32 -0400

    Cleanup before the rails

--- config.ru
+++
@@ -1,8 +0,0 @@
-
-require './bike_reports'
-
-# Workaround for the annoyting thin bug that prevents it from closing on a Ctrl-C
-trap('INT') { exit }
-
-run BikeReports::App
-
--- config/seed.rb
+++
@@ -1,4 +0,0 @@
-
-unless User.first(email: 'admin@fake.tld')
-  User.new(email: 'admin@fake.tld', password: 'password', password_confirmation: 'password', role: :admin).save
-end
