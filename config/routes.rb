3f5fbec38c43edfd8a959746612c0d7dd11358c8
Gabe Koss <gabe@gabekoss.com>
2013-10-11 21:25:47 -0400

    some more routes and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 BikeReports::Application.routes.draw do
   devise_for :users
   root 'pages#landing'
-  resources :bikes
+  resources :bikes do
+    member do
+      get :report_stolen, as: :report_stolen, action: :report_stolen
+    end
+  end
 end
