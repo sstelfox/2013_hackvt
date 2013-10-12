32db41a7455bf0bb63b28c1c201c503c04393a54
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 23:37:40 -0400

    First and last, moved helpers to helper, more incidents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def authenticate_user
+    unless current_user
+      redirect_to login_path, notice: "You need to login before continuing."
+    end
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
 end
