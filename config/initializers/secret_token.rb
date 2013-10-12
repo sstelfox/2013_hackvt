ebdb4d490cb4054bbbb1030c802a318bb9b71ec8
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 20:09:13 -0400

    AAaaannddd that should do it...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeReports::Application.config.secret_key_base = '2f300107e1c0e1ff987f14057a67f6a13541832b15e097393d18dc53dfba336c075fe7296881ef2500203405c2b7f3e5ca03561446e2013c600532e278fa99a6'
