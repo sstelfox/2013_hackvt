4ec31b66569d6282908be263fb907b3f1e482598
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-11 22:19:43 -0400

    Stuff be workin'

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -12,11 +12,17 @@ class RegistrationController < ApplicationController
     redirect_to root_path if current_user
 
     @registration_form = RegistrationForm.new
-    if @registration_form.submit(params[:registration])
+
+    if @registration_form.submit(registration_params)
       session[:user_id] = @registration_form.user.id
       redirect_to @registration_form.user, notice: "Thank you for registering!"
     else
       render "new"
     end
+  end
+
+  def registration_params
+    params.require(:registration)
+    params[:registration].permit(:email, :password, :password_confirmation)
   end
 end
